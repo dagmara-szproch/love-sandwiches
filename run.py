37167f3830d489812385f8b7a9604124e94d1f60
dagmara-szproch <dagmamara@gmail.com>
2025-08-06 11:36:11 +0100

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -117,14 +117,30 @@ def get_last_5_entries_sales():
     # pprint(columns)
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print('Calculating stock data..\n')
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = round(average * 1.1)
+        new_stock_data.append(stock_num)
+
+    return new_stock_data
+
 def main():
     data = get_sales_data()
     sales_data = [int(number) for number in data]
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_column = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_column)
+    update_worksheet(stock_data, 'stock')
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
+main()
 
-get_last_5_entries_sales()
